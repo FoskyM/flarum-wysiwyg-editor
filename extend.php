3dd5e4359e26e07d462fb03c97aa6aa5ad01f6eb
FoskyM <i@fosky.top>
2024-08-30 16:31:33 +0800

    export `parse`

--- extend.php
+++ extend.php
@@ -12,6 +12,7 @@
 namespace FoskyM\WysiwygEditor;
 
 use Flarum\Extend;
+use s9e\TextFormatter\Configurator;
 
 return [
     (new Extend\Frontend('forum'))
@@ -21,4 +22,9 @@ return [
         ->js(__DIR__.'/js/dist/admin.js')
         ->css(__DIR__.'/less/admin.less'),
     new Extend\Locales(__DIR__.'/locale'),
+
+    (new Extend\Formatter)
+        ->configure(function (Configurator $config) {
+            $config->javascript->exports = ['parse', 'preview'];
+        }),
 ];
